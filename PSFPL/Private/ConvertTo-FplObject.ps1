d3e23a59a4448b096615d93907bb26adf9e3f58f
sk82jack <sk82jack@users.noreply.github.com>
2018-11-24 00:11:08 +0000

    Add ConverTo-FplObject

---
+++ PSFPL/Private/ConvertTo-FplObject.ps1
@@ -0,0 +1,45 @@
+function ConvertTo-FplObject {
+    <#
+    .SYNOPSIS
+        Convert an object returned from the FPL API into a more PowerShelly object with a type name
+    .DESCRIPTION
+        Convert an object returned from the FPL API into a more PowerShelly object with a type name
+    .PARAMETER InputObject
+        A PowerShell object returned from the FPL API
+    .PARAMETER Type
+        The type name to give the resulted PowerShell object
+    .EXAMPLE
+        $Response = Invoke-RestMethod -Uri 'https://fantasy.premierleague.com/drf/elements/' -UseBasicParsing
+        ConvertTo-FplObject -InputObject $Response -Type 'FplPlayer'
+    .LINK
+        https://github.com/sk82jack/PSFPL/
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter()]
+        [object[]]
+        $InputObject,
+
+        [Parameter()]
+        [string]
+        $Type
+    )
+
+    $TextInfo = (Get-Culture).TextInfo
+    foreach ($Object in $InputObject) {
+        $Hashtable = @{}
+        $Object.psobject.properties | Foreach-Object {
+            $Name = $TextInfo.ToTitleCase($_.Name) -replace '_'
+            $Value = if ($_.Value -is [string]) {
+                [Text.Encoding]::UTF8.GetString([Text.Encoding]::GetEncoding('ISO-8859-1').GetBytes($_.Value))
+            }
+            else {
+                $_.Value
+            }
+            $Hashtable[$Name] = $Value
+        }
+        $FplObject = [pscustomobject]$Hashtable
+        $FplObject.PSObject.TypeNames.Insert(0, $Type)
+        $FplObject
+    }
+}
