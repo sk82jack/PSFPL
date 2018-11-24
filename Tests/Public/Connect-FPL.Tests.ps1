2818270adf69d2121af6969643257bf513bfb8bb
sk82jack <sk82jack@users.noreply.github.com>
2018-11-24 12:05:44 +0000

    Standardise test structure

--- Tests/Public/Connect-FPL.Tests.ps1
+++ Tests/Public/Connect-FPL.Tests.ps1
@@ -1,27 +1,28 @@
-$ModuleRoot = $env:BHPSModulePath
-. "$ModuleRoot\Public\Connect-FPL.ps1"
+Import-Module $ENV:BHPSModuleManifest -Force
 Describe 'Connect-FPL' {
-    BeforeAll {
-        $Password = ConvertTo-SecureString "password" -AsPlainText -Force
-        $Creds = [Management.Automation.PSCredential]::new("UserName", $Password)
-    }
-    Context 'Input' {
-        It 'Credential parameter is mandatory' {
-            (Get-Command Connect-FPL).Parameters['Credential'].Attributes.Mandatory | should be $true
-        }
-    }
-    Context 'Execution' {
+    InModuleScope 'PSFPL' {
         BeforeAll {
-            Mock Invoke-WebRequest {
-                [PSCustomObject]@{
-                    Headers = [PSCustomObject]@{
-                        'Set-Cookie' = $false
+            $Password = ConvertTo-SecureString "password" -AsPlainText -Force
+            $Creds = [Management.Automation.PSCredential]::new("UserName", $Password)
+        }
+        Context 'Input' {
+            It 'Credential parameter is mandatory' {
+                (Get-Command Connect-FPL).Parameters['Credential'].Attributes.Mandatory | should be $true
+            }
+        }
+        Context 'Execution' {
+            BeforeAll {
+                Mock Invoke-WebRequest {
+                    [PSCustomObject]@{
+                        Headers = [PSCustomObject]@{
+                            'Set-Cookie' = $false
+                        }
                     }
                 }
             }
-        }
-        It 'If incorrect credentials are given, throws a terminating error' {
-            {Connect-FPL -Credential $Creds} | Should -Throw
+            It 'If incorrect credentials are given, throws a terminating error' {
+                {Connect-FPL -Credential $Creds} | Should -Throw
+            }
         }
     }
 }
