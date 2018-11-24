f410cba292be3eaaf1fef8139eddc1c49122f845
sk82jack <sk82jack@users.noreply.github.com>
2018-11-24 11:09:47 +0000

    Add test for ConvertTo-FplObject

---
+++ Tests/ConvertTo-FplObject.Tests.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module $ENV:BHPSModuleManifest -Force
+Describe 'ConvertTo-FplObject' {
+    InModuleScope 'PSFPL' {
+        Context 'FplPlayer type' {
+            BeforeAll {
+                $Object = [PSCustomObject]@{
+                    web_name     = 'BellerÃ­n'
+                    element_type = 3
+                    team         = 1
+                    now_cost     = 58
+                }
+                Mock Get-FplElementType {
+                    @{
+                        1 = 'Goalkeeper'
+                        2 = 'Midfielder'
+                        3 = 'Defender'
+                        4 = 'Forward'
+                    }
+                }
+                Mock Get-FplClubId {
+                    @{
+                        1 = 'Arsenal'
+                        2 = 'Bournemouth'
+                    }
+                }
+                $Result = ConvertTo-FplObject -InputObject $Object -Type FplPlayer
+            }
+            It 'Converts diacritic characters' {
+                $Result.WebName | Should -Be 'Bellerin'
+            }
+            It 'Outputs an FplPlayer object' {
+                $Result.psobject.TypeNames | Should -Contain 'FplPlayer'
+            }
+            It 'Converts property names to Pascal Case' {
+                $Result.psobject.properties.Name | Should -Be @('WebName', 'Position', 'Price', 'Club', 'NowCost', 'ElementType', 'Team')
+            }
+            It 'Converts element type to position' {
+                $Result.Position | Should -Be 'Defender'
+            }
+            It 'Converts team ID to club name' {
+                $Result.Club | Should -Be 'Arsenal'
+            }
+        }
+    }
+}
