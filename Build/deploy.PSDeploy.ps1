22a60459a4f6c982dd8cba744449f8d90f801648
sk82jack <sk82jack@users.noreply.github.com>
2018-11-25 13:37:34 +0000

    Update psdeploy

--- Build/deploy.PSDeploy.ps1
+++ Build/deploy.PSDeploy.ps1
@@ -18,28 +18,24 @@
 # * Set-BuildEnvironment from BuildHelpers module has populated ENV:BHModulePath and related variables
 
 # Publish to gallery with a few restrictions
-if(
+if (
     $env:BHModulePath -and
     $env:BHBuildSystem -ne 'Unknown' -and
-    $env:BHBranchName -eq "master" -and
-    $env:BHCommitMessage -match '!deploy'
-)
-{
+    $env:BHBranchName -eq "master"
+) {
     Deploy Module {
         By PSGalleryModule {
             FromSource $ENV:BHModulePath
             To PHERepo
             WithOptions @{
-                ApiKey = $ENV:NugetApiKey
+                ApiKey = $ENV:PSREPO_APIKEY
             }
         }
     }
 }
-else
-{
+else {
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
-    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
-    "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName)" |
         Write-Host
 }
