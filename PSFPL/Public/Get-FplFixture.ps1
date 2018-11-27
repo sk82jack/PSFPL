9b287d92a5a90ef9e88aed4d1599503a2548615c
sk82jack <sk82jack@users.noreply.github.com>
2018-11-27 23:28:33 +0000

    Add parameters, sorting and filtering

--- PSFPL/Public/Get-FplFixture.ps1
+++ PSFPL/Public/Get-FplFixture.ps1
@@ -1,9 +1,24 @@
 function Get-FplFixture {
     [CmdletBinding()]
     Param (
+        [Parameter()]
+        [int]
+        $Gameweek,
 
+        [Parameter()]
+        [ValidateSet(
+            'Arsenal', 'Bournemouth', 'Brighton', 'Burnley', 'Cardiff', 'Chelsea', 'Crystal Palace',
+            'Everton', 'Fulham', 'Huddersfield', 'Leicester', 'Liverpool', 'Man City', 'Man Utd',
+            'Newcastle', 'Southampton', 'Spurs', 'Watford', 'West Ham', 'Wolves'
+        )]
+        [string]
+        $Club
     )
 
     $Response = Invoke-RestMethod -Uri 'https://fantasy.premierleague.com/drf/fixtures/' -UseBasicParsing
-    $Fixtures = ConvertTo-FplObject -InputObject $Response -Type 'FplFixture'
+    $Fixtures = ConvertTo-FplObject -InputObject $Response -Type 'FplFixture' | Sort-Object Gameweek, KickOffTime
+    $Fixtures.Where{
+        ($Gameweek -eq 0 -or $_.Gameweek -eq $Gameweek) -and
+        $_.ClubH + $_.ClubA -match $Club
+    }
 }
