a7136aa047f5e156e45c67ab9f4151f2a8a41a56
sk82jack <sk82jack@hotmail.com>
2018-12-27 20:29:16 +0000

    Add comment based help

--- PSFPL/Public/Get-FplTeam.ps1
+++ PSFPL/Public/Get-FplTeam.ps1
@@ -1,4 +1,25 @@
 function Get-FplTeam {
+    <#
+    .SYNOPSIS
+        Retrieve information about an FPL team
+    .DESCRIPTION
+        Retrieve information about an FPL team either by a specified team ID or get your own team by logging in with Connect-FPL
+    .PARAMETER TeamId
+        A team ID to retrieve information about
+    .EXAMPLE
+        Connect-Fpl -Email MyEmail@hotmail.com
+        $MyTeam = Get-FplTeam
+
+        Retrieves information about your own team
+    .EXAMPLE
+        Get-FplTeam -TeamId 12345
+
+        Retrieves information about the team with the ID 12345
+    .LINK
+        https://psfpl.readthedocs.io/en/latest/functions/Get-FplTeam
+    .LINK
+        https://github.com/sk82jack/PSFPL/blob/master/PSFPL/Public/Get-FplTeam.ps1
+    #>
     [CmdletBinding()]
     param (
         [Parameter()]
