2f7e766812d62e947f03dfe952b516ab82033f71
sk82jack <sk82jack@hotmail.com>
2019-01-24 11:19:18 +0000

    Add cbh for private functions

--- PSFPL/Private/Get-FplClubId.ps1
+++ PSFPL/Private/Get-FplClubId.ps1
@@ -1,4 +1,16 @@
 function Get-FplClubId {
+    <#
+    .SYNOPSIS
+        Retrieves a hashtable of club IDs to club names
+    .DESCRIPTION
+        Retrieves a hashtable of club IDs to club names
+    .EXAMPLE
+        Get-FplClubId
+    .LINK
+        https://github.com/sk82jack/PSFPL/
+    #>
+    [CmdletBinding()]
+    param ()
     $Response = Invoke-RestMethod -Uri 'https://fantasy.premierleague.com/drf/teams' -UseBasicParsing
     $Hashtable = @{}
     foreach ($Club in $Response) {
--- PSFPL/Private/Get-FplUserTeam.ps1
+++ PSFPL/Private/Get-FplUserTeam.ps1
@@ -1,4 +1,14 @@
 function Get-FplUserTeam {
+    <#
+    .SYNOPSIS
+        Retrieves the logged in users team
+    .DESCRIPTION
+        Retrieves the logged in users team
+    .EXAMPLE
+        Get-FplUserTeam
+    .LINK
+        https://github.com/sk82jack/PSFPL/
+    #>
     [CmdletBinding()]
     param ()
     $Response = Invoke-RestMethod -Uri 'https://fantasy.premierleague.com/drf/transfers' -UseDefaultCredentials -WebSession $Script:FplSession
