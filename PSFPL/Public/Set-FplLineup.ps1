dd7b0d91f497b919c05362e222e9e19f88090208
sk82jack <sk82jack@hotmail.com>
2019-02-21 00:59:01 +0000

    Add comment based help

--- PSFPL/Public/Set-FplLineup.ps1
+++ PSFPL/Public/Set-FplLineup.ps1
@@ -1,4 +1,45 @@
 function Set-FplLineup {
+    <#
+    .SYNOPSIS
+        Set your team lineup for the upcoming gameweek
+    .DESCRIPTION
+        Set your team lineup for the upcoming gameweek
+    .PARAMETER PlayersIn
+        The players which you wish to bring in to the starting XI.
+        Alternatively, if you are just swapping the order of players on your bench then use PlayersIn for one bench player and PlayersOut for the other
+    .PARAMETER PlayersOut
+        The players you wish to remove from the starting XI.
+        Alternatively, if you are just swapping the order of players on your bench then use PlayersIn for one bench player and PlayersOut for the other
+    .PARAMETER Captain
+        The player who you wish to be Captain of your team
+    .PARAMETER ViceCaptain
+        The player who you wish to be ViceCaptain of your team
+    .EXAMPLE
+        Set-FplLineup -PlayersIn Son -PlayersOut Pogba
+
+        This will remove Pogba from the starting XI and swap him with Son
+    .EXAMPLE
+        Set-FplLineup -PlayersIn Son, Rashford, Alexander-Arnold -PlayersOut Ings, Diop, Digne
+
+        This will remove Ings, Diop and Digne from the starting XI and swap them with Son, Rashford and Alexander-Arnold
+    .EXAMPLE
+        Set-FplLineup -Captain Salah
+
+        This will set your captain for the upcoming gameweek to Salah
+    .EXAMPLE
+        Set-FplLineup -ViceCaptain Sterling
+
+        This will set your vice captain for the upcoming gameweek to Sterling
+    .EXAMPLE
+        Set-FplLinup -PlayersIn Sane -PlayersOut Diop -Captain Sane -ViceCaptain Pogba
+
+        This will swap out Diop for Sane in your starting XI and then set Sane to be the captain and Pogba to be the vice captain
+    .LINK
+        https://psfpl.readthedocs.io/en/master/functions/Set-FplLineup
+    .LINK
+        https://github.com/sk82jack/PSFPL/blob/master/PSFPL/Public/Set-FplLineup.ps1
+    #>
+
     [CmdletBinding()]
     param (
         [Parameter()]
