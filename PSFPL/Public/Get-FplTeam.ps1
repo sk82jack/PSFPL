2cddaf63f205739e33ee34bf768866ebd3d76d91
sk82jack <sk82jack@users.noreply.github.com>
2019-04-05 23:52:02 +0100

    Remove trailing slash

--- PSFPL/Public/Get-FplTeam.ps1
+++ PSFPL/Public/Get-FplTeam.ps1
@@ -28,7 +28,7 @@ function Get-FplTeam {
     )
     process {
         if ($TeamId -gt 0) {
-            $Response = Invoke-RestMethod -Uri "https://fantasy.premierleague.com/drf/entry/$TeamId/" -UseDefaultCredentials
+            $Response = Invoke-RestMethod -Uri "https://fantasy.premierleague.com/drf/entry/$TeamId" -UseDefaultCredentials
             if ($Response -match 'The game is being updated.') {
                 Write-Warning 'The game is being updated. Please try again shortly.'
                 return
